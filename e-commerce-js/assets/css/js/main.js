9912fc643ac2ce520564dd1dd76a33af2c1ddcf2
harshdev26 <156781178+harshdev26@users.noreply.github.com>
2025-07-06 16:20:08 +0000

    filestructure

---
+++ e-commerce-js/assets/css/js/main.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("product-container");
+
+products.forEach(function(product){
+   let card = document.createElement("div");
+   card.className = "product-card"
+   
+   card.innerHTML = `
+    <img src="${product.image}" alt="${product.name}" />
+    <h3>${product.name}</h3> 
+    <p>${product.price}</p>
+    <p>${product.description}</p>
+    <button></button>`
+       
+   document.getElementById("product-container").appendChild(card);
+
+}); 
+
+
---
+++ e-commerce-js/data/product.js
@@ -0,0 +1,29 @@
+// data/products.js
+const products = [
+  {
+    id: 1,
+    name: "Men's T-Shirt",
+    price: 499,
+    image: "https://via.placeholder.com/200x200?text=T-Shirt",
+    description: "Comfortable cotton t-shirt for men"
+  },
+  {
+    id: 2,
+    name: "Women's Hoodie",
+    price: 999,
+    image: "https://via.placeholder.com/200x200?text=Hoodie",
+    description: "Warm and cozy hoodie for women"
+  },
+  {
+    id: 3,
+    name: "Running Shoes",
+    price: 1499,
+    image: "https://via.placeholder.com/200x200?text=Shoes",
+    description: "Lightweight running shoes"
+  }
+];
+
+
+// products.forEach(function(product){
+//    console.log(product);
+// })
